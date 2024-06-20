dc412b968d41ce8996aa74c9b26098cca9293887
Isabelle Albuquerque <122169292+IsabelleAlb@users.noreply.github.com>
2024-06-20 04:24:25 -0300

    Add files via upload
    É ESSE AQUI, IAN

--- Irriga.iot.ino
+++ Irriga.iot.ino
@@ -2,25 +2,25 @@
 #include <ThingSpeak.h>
 
 //id e senha da rede
-const char* ssid = "Isa";
-const char* password =  "isabelle123";
+const char* ssid = "Marluce";
+const char* password =  "Wifi8826";
 WiFiClient client;
 
 
 //chaves de conexão do thingspeak
-const long CHANNEL = "2567533";
+const long CHANNEL = 2567533;
 const char *WRITE_API = "C0AESWMJU4GDDL0G";
 const char *READ_API = "D4IH5OS02S8CZTUD";
 int intervaloTempo = 15000; // intervalo de tempo para escrever no Thing Speak write
-unsigned long tempoAnterior, tempoAgora;
+unsigned long tempoAnterior;
 
 
 //pinos, variáveis e constantes do módulo 
-#define sensorUmidade 0 
-#define ledVermelho 
-#define ledAmarelo 
-#define bomba 
-const int limiteUmidade = 72; //umidade ideal do solo
+#define sensorUmidade 32
+#define ledVermelho 19
+#define ledAmarelo 18
+#define bomba 5
+const int limiteUmidade = 40; //umidade ideal do solo
 int umidadeSolo = 0;
 bool acionar = 0;
 
@@ -35,8 +35,8 @@ void setup() {
   Serial.begin(9600);
   
   if (WiFi.status() !=  WL_CONNECTED) {
-    Serial.print("Conectando à rede ")
-    Serial.println(ssid)
+    Serial.print("Conectando à rede ");
+    Serial.println(ssid);
     while (WiFi.status() != WL_CONNECTED) {
       WiFi.begin(ssid, password);
       Serial.println("Conectando...");
@@ -44,7 +44,7 @@ void setup() {
   }  
   Serial.println("Conexão bem sucedida!");
  }
-  tempoAnterio = 0;
+  tempoAnterior = 0;
   WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client); //inicializa o ThingSpeak 
 
@@ -53,7 +53,7 @@ void setup() {
 void leituraSensor (){
     
     umidadeSolo = analogRead(sensorUmidade); //faz a leitura do sensor de umidade do solo
-    umidadeSolo = map(umidadeSolo, 1023, 0, 0, 100); //converte a variação do sensor de 0 a 1023 para 0 a 100
+    umidadeSolo = map(umidadeSolo, 4095, 0, 0, 100); //converte a variação do sensor de 0 a 1023 para 0 a 100
     //mostra a mensagem
     Serial.println("Umidade: ");
     Serial.print(umidadeSolo); 
@@ -64,15 +64,17 @@ void leituraSensor (){
 void bombear(){
   if (umidadeSolo < limiteUmidade){
     Serial.println("Regando...");
-    digitalWrite(ledVermelho, HIGH);
-    digitalWrite(ledAmarelo, LOW);
-    digitalWrite(bomba, HIGH);
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledAmarelo, HIGH);
+    digitalWrite(bomba, LOW);
+    delay(8000);
+    digitalWrite(bomba,HIGH);
     acionar = 1;
   }
   else{
     Serial.println("Umidade ideal!");
-    digitalWrite(ledVermelho, LOW);
-    digitalWrite(ledAmarelo, HIGH);
+    digitalWrite(ledVermelho, HIGH);
+    digitalWrite(ledAmarelo, LOW);
     digitalWrite(bomba, LOW);
   }
 }
@@ -80,21 +82,23 @@ void bombear(){
 
 void loop() { 
 
-  tempoAgora = millis();
-  if ((tempoAgora - tempoAnterior > intervaloTempo)){
-    ThingSpeak.setField(1, umidadeSolo);
-    ThingSpeak.setField(2, acionar);
+pinMode(bomba, HIGH);  
+  if ((millis() - tempoAnterior > intervaloTempo)){
     leituraSensor();
     bombear();
-    acionar = 0;
-  }
-  int x = ThingSpeak.writeFields(CHANNEL, WRITE_API);
+    ThingSpeak.setField(1, umidadeSolo);
+    if (acionar == 1){
+      ThingSpeak.setField(2, acionar);
+      acionar = 0;
+    }
+    tempoAnterior = millis();
+    int x = ThingSpeak.writeFields(CHANNEL, WRITE_API);
     if (x == 200) {
-      Serial,println("Update realizado!");
-  }
+      Serial.println("Update realizado!");
+    }
     else {
       Serial.println("Problema no canal - erro HTTP" + String (x));
+    }
   }
-  
-
+  delay(10000);
 }
